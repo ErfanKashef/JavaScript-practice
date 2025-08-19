cf1a5c0b1d974e640bfc9afb73c2f1a0631e9a52
ErfanKashef <129071476+ErfanKashef@users.noreply.github.com>
2025-08-19 14:39:06 +0330

    array sort

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ const Flattening = [1, [2, [3, [4]], 5]];
 const Newflattening = Flattening.flat(3);
 
 console.log(Newflattening);
+
+// Array Sort
+
+function sortNumber(arr) {
+  return arr.sort((a, b) => a - b);
+}
+
+console.log(sortNumber([3, 1, 2]));
+
+// Array Sort
+
+function sortByKey(arr) {
+  return arr.sort((a, b) => a - b);
+}
+const number = [5, 2, 1, 3, 4];
+console.log(sortByKey(number));
