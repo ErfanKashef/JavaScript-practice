f97a7407b76bd332f1dcb7e14ef16ef96625b116
ErfanKashef <129071476+ErfanKashef@users.noreply.github.com>
2025-08-19 14:12:33 +0330

    Array Flattening

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+// return only even numbers.
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const result = numbers.filter((number) => number % 2 === 0);
 
 console.log(result);
+
+// Array Flattening
+const Flattening = [1, [2, [3, [4]], 5]];
+
+const Newflattening = Flattening.flat(3);
+
+console.log(Newflattening);
